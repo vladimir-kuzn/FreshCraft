40a0da58c7e1c80e820795119deaeb9da5b8405f
Pheonix <vova.kuzz2000@gmail.com>
2023-01-16 20:47:27 +0300

    Mail confirmation enabled

--- routes/web.php
+++ routes/web.php
@@ -54,5 +54,6 @@ Route::name('admin.')->middleware([Autorized::class])->group( function(){
 
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
